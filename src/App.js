ba86f0df5aa700744c1dc71f3fd70bf61d3c1238
Tashi <tashishyangba23@gmail.com>
2023-06-20 22:10:56 -0400

    linked pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+//import components
+import Navbar from "./components/Navbar";
+//import pages
+import { Credit, Debit, Home, UserProfile } from "./pages";
 function App() {
   return (
+    <Router> 
     <div className="App">
+      {/* Navigation */}
+      <Navbar/>
     </div>
+    {/* Routes */}
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/userProfile/*" element={<UserProfile />} />
+        <Route path="/userProfile/debit" element={<Debit />} />
+        <Route path="/userProfile/credit" element={<Credit />} />
+    </Routes>
+    </Router>
+  
   );
 }
 
---
+++ src/pages/Debit.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Debit=()=>{
+    return(
+        <h1>Debit</h1>
+    );
+}
+
+export default Debit;
