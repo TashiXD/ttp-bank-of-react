f5d35d7454619697db8fae0b85153001bcce6c42
Tashi <tashishyangba23@gmail.com>
2023-06-21 15:16:19 -0400

    made broswerRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 //import components
 import { Navbar } from "./components";
